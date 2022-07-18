9c58a1cd089e97d1f35e6a352c81ffe5decbd2ee
NISHANT KUMAR <85857415+nishant-lang@users.noreply.github.com>
2022-07-18 14:38:56 +0530

    Update 0002_rename_user_userprofile.py

--- userprofile/migrations/0002_rename_user_userprofile.py
+++ userprofile/migrations/0002_rename_user_userprofile.py
@@ -4,7 +4,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-    atomic=False
+    
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
         ('admin', '0003_logentry_add_action_flag_choices'),
